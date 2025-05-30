73c915c57067d61d3cdf526d3f19b7ec5905a95e
DIinesh Kanna R <dineshkannaravi25@gmail.com>
2025-05-31 00:59:23 +0530

    day1

---
+++ src/swapping_two_elements.java
@@ -0,0 +1,23 @@
+public class swapping_two_elements {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int temp=0;
+        System.out.println("before swapping "+a+ " and  "+b  );
+        temp=a;
+        a=b;
+        b=temp;
+
+        //2nd
+        //a=a+b;
+        //b=a-b;
+         //a=a-b;
+
+        //3rd
+         //a=a*b;
+         //b=a/b;
+        //a=a/b;
+        System.out.println("before swapping "+a +" and "+b);
+
+    }
+}
