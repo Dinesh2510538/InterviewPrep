e170de3b767c71b40b41aa3f37caaaa6f403b9d5
DIinesh Kanna R <dineshkannaravi25@gmail.com>
2025-07-09 23:31:08 +0530

    day18

---
+++ src/july9/Panagram.java
@@ -0,0 +1,22 @@
+package july9;
+
+public class Panagram {
+    public static void main(String[] args) {
+        String str = "The quick brown fox jumps over the lay dog";
+        str = str.toLowerCase();
+
+        if (str.length() < 26) {
+            System.out.println("Not a pangram");
+            return;
+        }
+
+        for (char c = 'a'; c <= 'z'; c++) {
+            if (str.indexOf(c) < 0) {
+                System.out.println("Not a pangram");
+                return;
+            }
+        }
+
+        System.out.println("It is a pangram");
+    }
+}
