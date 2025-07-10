359362ce6e9e051083cb39e3dce0696e94b45b08
DIinesh Kanna R <dineshkannaravi25@gmail.com>
2025-07-10 23:58:27 +0530

    day19

---
+++ src/july10/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,17 @@
+package july10;
+
+public class BestTimeToBuyAndSellStock {
+    public static void main(String[] args) {
+        int[]prices={7,1,5,3,6,4};
+        int buy = prices[0];
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < buy) {
+                buy = prices[i];
+            } else if (prices[i] - buy > profit) {
+                profit = prices[i] - buy;
+            }
+        }
+        System.out.println(profit);
+    }
+}
