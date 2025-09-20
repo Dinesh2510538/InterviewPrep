465378d6212644080c9c1559c6fee8b7079aa903
DIinesh Kanna R <dineshkannaravi25@gmail.com>
2025-09-20 23:52:09 +0530

    day28

---
+++ src/sep20/count.java
@@ -0,0 +1,13 @@
+package sep20;
+
+public class count {
+    public static void main(String[] args) {
+        int number=1234;
+        int count=0;
+        while (number!=0){
+            number=number/10;
+            count++;
+        }
+        System.out.println("number of digits: "+count);
+    }
+}
