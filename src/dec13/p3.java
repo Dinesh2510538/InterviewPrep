f836abf00964ae9eb27407cf3138ac867c5991d5
DIinesh Kanna R <dineshkannaravi25@gmail.com>
2025-12-13 23:55:47 +0530

    day9

---
+++ src/dec13/p3.java
@@ -0,0 +1,17 @@
+package dec13;
+
+public class p3 {
+    static void main() {
+        char ch = 'A';
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(ch + " ");
+
+            }
+
+
+            System.out.println();
+            ch++;
+        }
+    }
+}
