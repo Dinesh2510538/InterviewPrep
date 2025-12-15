870af033ec5d9ede7faa35e9c07dec31be42d2a6
DIinesh Kanna R <dineshkannaravi25@gmail.com>
2025-12-15 22:46:18 +0530

    day11

---
+++ src/decc15/PerfectNumber.java
@@ -0,0 +1,16 @@
+package decc15;
+
+class PerfectNumber {
+    public static void main(String[] args) {
+        int n = 28, sum = 0;
+
+        for (int i = 1; i <= n / 2; i++)
+            if (n % i == 0)
+                sum += i;
+
+        if (sum == n)
+            System.out.println("Perfect Number");
+        else
+            System.out.println("Not Perfect");
+    }
+}
