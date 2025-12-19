cbd54e10ebd43333f7338b4eaf83e437580e8d6d
DIinesh Kanna R <dineshkannaravi25@gmail.com>
2025-12-19 22:29:21 +0530

    day13

---
+++ src/dec19/RightRotate.java
@@ -0,0 +1,16 @@
+package dec19;
+
+class RightRotate {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4};
+        int last = a[a.length - 1];
+
+        for (int i = a.length - 1; i > 0; i--)
+            a[i] = a[i - 1];
+
+        a[0] = last;
+
+        for (int x : a)
+            System.out.print(x + " ");
+    }
+}
