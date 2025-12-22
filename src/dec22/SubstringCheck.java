f7f8f3aa60ee587486b8d5789fa6468281afe722
DIinesh Kanna R <dineshkannaravi25@gmail.com>
2025-12-22 23:29:41 +0530

    day15

---
+++ src/dec22/SubstringCheck.java
@@ -0,0 +1,9 @@
+package dec22;
+
+class SubstringCheck {
+    public static void main(String[] args) {
+        String s1 = "programming";
+        String s2 = "gram";
+        System.out.println(s1.contains(s2));
+    }
+}
